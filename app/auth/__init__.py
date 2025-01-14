ccea110612540bd502a7e1420eb35361f846eb43
anselbrandt <ansel@anselbrandt.com>
2025-01-13 23:52:14 -0800

    Add CurrentUser

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,4 +1,5 @@
 from .auth import getGithubUser, getGoogleUser, getMicrosoftUser
 from .tokens import create_jwt
+from .user import CurrentUser
 
-__all__ = [getGithubUser, getGoogleUser, getMicrosoftUser, create_jwt]
+__all__ = [getGithubUser, getGoogleUser, getMicrosoftUser, create_jwt, CurrentUser]
--- app/routers/user_routes.py
+++ app/routers/user_routes.py
@@ -3,10 +3,9 @@ from typing import Optional
 from fastapi import APIRouter, Header, Request, Response
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-import jwt
 
-from app.constants import ROOT_PATH, COOKIE_NAME, TOKEN_SECRET, TOKEN_ALGORITHM
-from utils import generateFruitname
+from app.constants import ROOT_PATH
+from app.auth import CurrentUser
 
 router = APIRouter(prefix="/user")
 
@@ -15,17 +14,17 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/profile", response_class=HTMLResponse)
 async def user_profile(
-    request: Request, response: Response, hx_request: Optional[str] = Header(None)
+    user: CurrentUser,
+    request: Request,
+    response: Response,
+    hx_request: Optional[str] = Header(None),
 ):
-    token = request.cookies.get(COOKIE_NAME)
-    if token is None:
+    if user is None:
         context = {
             "request": request,
             "root_path": ROOT_PATH,
-            "username": generateFruitname(),
         }
-        return templates.TemplateResponse("profile.html", context)
-    user = jwt.decode(token, TOKEN_SECRET, algorithms=[TOKEN_ALGORITHM])
+        return templates.TemplateResponse("login.html", context)
     context = {
         "request": request,
         "root_path": ROOT_PATH,
